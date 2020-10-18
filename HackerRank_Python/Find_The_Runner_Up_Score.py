6103b86e34b311a9a983b927976ebb4d48a48988
Chanchal Kumar Maurya <73050886+striver005@users.noreply.github.com>
2020-10-18 11:51:12 +0530

    Update Find_The_Runner_Up_Score.py
    Added contents to Find_The_Runner_Up_Score.py

--- HackerRank_Python/Find_The_Runner_Up_Score.py
+++ HackerRank_Python/Find_The_Runner_Up_Score.py
@@ -1,1 +1,16 @@
+if __name__ == '__main__':
+    #Users Input
+    n = int(raw_input())
+    arr = map(int, raw_input().split())
+    
+    #declare the winners score as maxx and runner up score as runner_up
+    maxx = max(arr)
+    runner_up = min(arr)
 
+    #update runner_up
+    for i in arr:
+        if (i < maxx) and (i > runner_up):
+            runner_up = i
+    
+    #display the runner up score
+    print(runner_up)
